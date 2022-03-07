c581631e90158d4bbdde21cc9026a44a3120d081
vedanshi.mehta <vedanshi.mehta@1R-76>
2022-03-07 17:17:33 +0530

    Add project files.

---
+++ LinqLambaEx/CarData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLambaEx
+{
+    enum Color
+    {
+        Red, Blue, Green, Yellow, White, Orange, Black
+    }
+
+    class CarData
+    {
+       
+
+        public int carId { get; set; }
+        public string carName { get; set; }
+
+        public Color carColors{ get; set; }
+
+        public string carPrice { get; set; }
+    }
+
+
+    /* List<CarData> cars = new List<CarData>()
+            {
+                new CarData{carId = 01, carName="BMW X5" , carColors= Color.Blue, carPrice = "92.90Lakh"},
+                new CarData{carId= 02, carName="Range Rover", carColors= Color.Blue, carPrice = "66.60Lakh"},
+                new CarData{carId= 03, carName="Rolls Royce Dawn", carColors= Color.Black, carPrice = "7.64Crore"},
+                new CarData{carId= 04, carName="Volvo X60", carColors= Color.White, carPrice = "1.2Crore"},
+                new CarData{carId= 05, carName="Skoda Superb", carColors= Color.Red, carPrice = "32.85Lakh"},
+                new CarData{carId= 06, carName="Volkswagen Taigun", carColors= Color.Yellow, carPrice = "17.99Lakh"},
+                new CarData{carId= 07, carName="Mahindra XUV700", carColors= Color.Red, carPrice = "17.79Lakh"},
+                new CarData{carId= 08, carName="Maruti Ertiga", carColors= Color.Orange, carPrice = "10.85Lakh"},
+                new CarData{carId= 09, carName="Tata Altroz", carColors= Color.Yellow, carPrice = "8.99Lakh"},
+                new CarData{carId= 10, carName="Maruti Alto", carColors= Color.Black, carPrice = "3.25Lakh"},
+                new CarData{carId= 11, carName="Audi etron GT", carColors= Color.Blue, carPrice = "1.7Crore"},
+
+
+            };*/
+}
---
+++ LinqLambaEx/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLambaEx
+{
+    class Program
+    {
+        public static List<string> cities;
+
+
+        static void Main(string[] args)
+        {
+
+
+            List<string> cities = new List<string>()
+            {
+
+                "Mumbai",
+                "Delhi",
+                "Ahmedabad",
+                "Surat",
+                "Valsad",
+                "Varnasi",
+                "Chennai",
+                "Bhopal",
+                "Darjeeling"
+
+
+            };
+
+            //1)Cities in decending  order
+            var citiesDescendingLinq = (from city in cities
+                                       orderby city descending
+                                       select city).ToList();
+
+            var citiesDecendingLambda = cities.OrderByDescending(city => city).ToList();
+
+
+            //2) Cities in ascending order
+            var citiesAscendingLinq = (from city in cities
+                                        orderby city 
+                                        select city).ToList();
+
+            var citiesAscendingLambda = cities.OrderBy(city => city).ToList();
+
+            //3) Cities in descending order by its length
+            var citiesDescendingLenghtLinq = (from city in cities
+                                               orderby city.Length descending
+                                                select city).ToList();
+
+            var citiesDescendingLenghtLambda = cities.OrderByDescending(city => city.Length).ToList();
+
+
+
+
+            //4)Cities in ascending order by its length
+            var citiesAscendingLenghtLinq = (from city in cities
+                                              orderby city.Length 
+                                              select city).ToList();
+
+            var citiesAscendingLenghtLambda = cities.OrderBy(city => city.Length).ToList();
+
+
+            //5)Get cities names starts with V and D letter.
+            var citiesVandDLinq = (from city in cities
+                               where city.StartsWith("V") || city.StartsWith("D")
+                               select city).ToList();
+
+            var citiesVandDLambda = cities.Where(city => city.StartsWith("V") || city.StartsWith("D")).ToList();
+
+            //6)Get count of city names starts with A letter.
+            var citiesCountALinq = (from city in cities
+                                   where city.StartsWith("A") 
+                                   select city).Count();
+
+            var citiesCountALambda = cities.Count(city => city.StartsWith("A"));
+
+            //7)Group ordered city names by its first letter
+            var citiesOrderedFirstLetterLinq = from city in cities
+                                               group city by city[0] into cityGroup
+                                               orderby cityGroup.Key
+                                               select cityGroup;
+
+            var citiesOrderedFirstLetterLambda = cities.GroupBy(city => city[0]).OrderBy(city => city.Key);
+
+            GetDetails(citiesDescendingLinq);
+
+            Console.WriteLine("City Name that start with A is: " + citiesCountALinq);
+            Console.WriteLine("City Name that start with A is: " + citiesCountALambda);
+
+            foreach (var cityGroup in citiesOrderedFirstLetterLambda)
+            {
+                Console.WriteLine(cityGroup.Key);
+
+                foreach(var city in cityGroup)
+                {
+
+                    Console.WriteLine(city);
+                }
+               
+            }
+            Console.ReadKey();
+
+
+        }
+
+        public static void GetDetails(List<string> cities)
+        {
+
+
+            foreach (var city in cities)
+            {
+               
+                    Console.WriteLine(city);
+                  
+
+
+                
+            }
+        
+        }
+
+      
+    }
+}
